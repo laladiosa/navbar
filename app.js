1ea156dd538f1d22ff26c9e5db5959cd91eb4be8
laladiosa <hihi.cod@gmail.com>
2022-05-23 16:43:40 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+// classList - show/gets all classes in the
+//contains - check classlist for specific class
+// add - add class
+//remove - remove class
+//toggle - toggle classes
+
+const navToggle = document.querySelector('.nav-toggle');
+const links = document.querySelector('.links');
+
+
+navToggle.addEventListener('click', function() {
+
+    // if (links.classList.contains('show-links')) {
+    //     links.classList.remove('show-links');
+    // } else {
+    //     links.classList.add('show-links');
+    // }
+
+    links.classList.toggle('show-links');
+
+})
